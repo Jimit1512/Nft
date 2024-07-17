1097bd0da2cd4782eefdb597d5b73261aaf57964
Syed Asad Kazmi <sak786@Syeds-MacBook-Air.local>
2024-07-17 06:17:46 +0530

    Fixed errors in randomIpfsNft.test.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,18 +9,18 @@ require("dotenv").config();
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const COIMMASTER_API_KEY = process.env.COIMMASTER_API_KEY;
-const ETHERSCAN_API_KEY=process.env.ETHER_API;
+const ETHERSCAN_API_KEY = process.env.ETHER_API;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   sourcify: {
     enabled: true,
   },
-  etherscan:{
+  etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
   defaultNetwork: "hardhat",
-  networks:{
+  networks: {
     hardhat: {
       chainId: 31337,
     },
@@ -28,7 +28,7 @@ module.exports = {
       chainId: 31337,
     },
     sepolia: {
-      chainId:11155111,
+      chainId: 11155111,
       blockConfirmation: 1,
       url: SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
@@ -36,25 +36,25 @@ module.exports = {
   },
   solidity: {
     compilers: [
-        {
-          version: "0.8.0",
-        },
-        {
-          version: "0.8.4",
-        },
-        {
-            version: "0.8.7",
-        },
-        {
-            version: "0.8.20",
-        },
-        {
-          version: "0.8.24",
-        },
+      {
+        version: "0.8.0",
+      },
+      {
+        version: "0.8.4",
+      },
+      {
+        version: "0.8.7",
+      },
+      {
+        version: "0.8.20",
+      },
+      {
+        version: "0.8.24",
+      },
     ],
-},
+  },
   namedAccounts: {
-    deployer:{
+    deployer: {
       default: 0,
     },
     player: {
